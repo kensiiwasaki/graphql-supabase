b13e6ffabe0ee2312f46feb6ce0d2cbfb3740287
kensiiwasaki <kensiiwasaki@gmail.com>
2023-03-03 00:25:49 +0900

    add: メインページ

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,45 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import {
+  ApolloClient,
+  ApolloProvider,
+  createHttpLink,
+  InMemoryCache,
+} from '@apollo/client'
+import { supabase, supabaseAnonKey, supabaseUrl } from '../src/constants'
+import { setContext } from '@apollo/client/link/context'
+
+const httpLink = createHttpLink({
+  uri: `${supabaseUrl}/graphql/v1`,
+})
+
+const authLink = setContext(async (_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const session = (await supabase.auth.getSession()).data.session
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: `Bearer ${
+        session ? session.access_token : supabaseAnonKey
+      }`,
+      apikey: supabaseAnonKey,
+    },
+  }
+})
+
+const apolloClient = new ApolloClient({
+  uri: `${supabaseUrl}/graphql/v1`,
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+})
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ApolloProvider client={apolloClient}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  )
 }
 
 export default MyApp
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,86 +1,212 @@
+import { Session } from '@supabase/supabase-js'
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
+import React, { useEffect, useState } from 'react'
+import { useMutation, useQuery } from '@apollo/client'
+import { OrderByDirection } from '../src/__generated__/graphql'
+import { supabase, taskInsert, tasksQuery, taskUpdate } from '../src/constants'
 
 const Home: NextPage = () => {
+  const [session, setSession] = useState<Session | null>(null)
+
+  useEffect(() => {
+    const getInitialSession = async () => {
+      const initialSession = (await supabase.auth.getSession())?.data.session
+      setSession(initialSession)
+    }
+
+    getInitialSession()
+
+    const authListener = supabase.auth.onAuthStateChange(
+      async (event, session) => {
+        setSession(session)
+      }
+    )
+
+    return () => {
+      authListener.data.subscription.unsubscribe()
+    }
+  }, [])
+
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div className="flex flex-col bg-black h-screen">
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Supabase pg_graphql Example</title>
       </Head>
 
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
+      <AppHeader isSignedIn={!!session} />
 
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.tsx
-          </code>
-        </p>
-
-        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and its API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+      <main className="text-white flex-grow max-w-4xl mx-auto min-h-0">
+        {session ? <TodoList /> : <LoginForm />}
       </main>
-
-      <footer className="flex h-24 w-full items-center justify-center border-t">
-        <a
-          className="flex items-center justify-center gap-2"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-        </a>
-      </footer>
     </div>
   )
 }
 
 export default Home
+
+const TodoList = (): JSX.Element => {
+  const {
+    loading,
+    data: queryData,
+    refetch: refetchTasks,
+  } = useQuery(tasksQuery, {
+    variables: {
+      orderBy: [
+        {
+          created_at: OrderByDirection.DescNullsFirst,
+        },
+      ],
+    },
+  })
+
+  const [insertTask, { loading: mutationLoading }] = useMutation(taskInsert)
+
+  const [updateTask, { loading: updateLoading }] = useMutation(taskUpdate)
+
+  const onSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    const formElement = event.currentTarget
+    event.preventDefault()
+    const { title } = Object.fromEntries(new FormData(event.currentTarget))
+    if (typeof title !== 'string') return
+    if (!title) return
+    await insertTask({
+      variables: {
+        objects: [{ title }],
+      },
+      onCompleted: () => refetchTasks(),
+    })
+    formElement.reset()
+  }
+
+  const toggleTaskStatus = async (taskId: string, updatedStatus: boolean) => {
+    await updateTask({
+      variables: {
+        set: {
+          is_completed: updatedStatus,
+        },
+        filter: {
+          id: {
+            eq: taskId,
+          },
+        },
+      },
+      onCompleted: () => refetchTasks(),
+    })
+  }
+
+  if (loading) {
+    return <div>Loading</div>
+  }
+
+  const tasks = queryData!.tasksCollection!.edges
+  return (
+    <div className="h-full flex flex-col">
+      <div className="flex-grow min-h-0 overflow-y-auto">
+        {tasks.map((task) => (
+          <div key={task.node.id} className="text-lg p-1 flex">
+            <div className="flex-grow">{task.node.title}</div>
+            <button
+              className="px-2"
+              onClick={() =>
+                toggleTaskStatus(task.node.id, !task.node.is_completed)
+              }
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className={`w-6 h-6 ${
+                  task.node.is_completed
+                    ? 'stroke-green-500'
+                    : 'stroke-gray-500'
+                }`}
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                />
+              </svg>
+            </button>
+          </div>
+        ))}
+      </div>
+      <form className="flex items-center p-1" onSubmit={onSubmit}>
+        <input
+          className="border-green-300 border bg-transparent rounded py-1 px-2 flex-grow mr-2"
+          type="title"
+          name="title"
+          placeholder="New Task"
+        />
+        <button
+          type="submit"
+          disabled={mutationLoading}
+          className="py-1 px-4 text-lg bg-green-400 rounded text-black disabled:bg-gray-500"
+        >
+          {mutationLoading ? 'Saving...' : 'Save'}
+        </button>
+      </form>
+    </div>
+  )
+}
+
+const LoginForm = () => {
+  const sendMagicLink = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    const { email } = Object.fromEntries(new FormData(event.currentTarget))
+    if (typeof email !== 'string') return
+
+    const { error } = await supabase.auth.signInWithOtp({ email })
+    if (error) {
+      console.log(error)
+      alert(error.message)
+    } else {
+      alert('Check your email inbox')
+    }
+  }
+
+  return (
+    <form
+      className="flex flex-col justify-center space-y-2 max-w-md px-4 h-full"
+      onSubmit={sendMagicLink}
+    >
+      <input
+        className="border-green-300 border rounded p-2 bg-transparent text-white"
+        type="email"
+        name="email"
+        placeholder="Email"
+      />
+      <button
+        type="submit"
+        className="py-1 px-4 text-lg bg-green-400 rounded text-black"
+      >
+        Send Magic Link
+      </button>
+    </form>
+  )
+}
+
+/** 上の`Home`をスッキリさせるためだけに抜き出したヘッダー */
+const AppHeader = ({ isSignedIn }: { isSignedIn: boolean }) => {
+  console.log({ isSignedIn })
+  return (
+    <header className="bg-black shadow shadow-green-400 px-4">
+      <div className="flex max-w-4xl mx-auto items-center h-16">
+        <div className=" text-white text-lg flex-grow">
+          Supabase pg_graphql Example
+        </div>
+        {isSignedIn && (
+          <button
+            className="py-1 px-2 text-white border border-white rounded"
+            onClick={() => supabase.auth.signOut()}
+          >
+            Sign Out
+          </button>
+        )}
+      </div>
+    </header>
+  )
+}
